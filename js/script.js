4988f23e84969cf0cc7aac6ce2c59a269c33c359
pvdberssel <pim.bersselaar@live.com>
2024-10-18 19:37:34 +0200

    Add display book button functionality with grid

--- js/script.js
+++ js/script.js
@@ -9,7 +9,35 @@ function Book(bookTitle,bookAuthor,bookPages,bookRead){
 }
 
 myLibrary.displayBooks = function displayBooks(){
-    this.forEach((book) => console.log(book))
+    const bookContainer = document.querySelector(".book-container");
+    this.forEach((book) => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("bookcard")
+
+        const divTitle = document.createElement("div");
+        divTitle.classList.add('booktitle');
+        divTitle.textContent = book.title;
+        bookCard.appendChild(divTitle);
+
+        const divAuthor = document.createElement("div");
+        divAuthor.classList.add('bookauthor');
+        divAuthor.textContent = book.author;
+        bookCard.appendChild(divAuthor);
+
+        const divPages = document.createElement("div");
+        divPages.classList.add('bookpages');
+        divPages.textContent = book.pages;
+        bookCard.appendChild(divPages);
+
+        const divRead = document.createElement("button");
+        divRead.classList.add('bookread');
+        divRead.textContent = book.pages;
+        bookCard.appendChild(divRead);
+
+        bookContainer.appendChild(bookCard)
+        })
+    
+    
 }
 
 Book.prototype.addBookToLibrary = function addBookToLibrary(myLibrary){
@@ -21,11 +49,20 @@ Book.prototype.removeBookFromLibrary = function removeBookFromLibrary(myLibrary)
     myLibrary.splice(bookIndex,1)
 }
 
-myLibrary.displayBooks();
+
+
+
+const displayButton = document.querySelector(".display-book");
+displayButton.addEventListener('click',() => {
+    console.log('click')
+    myLibrary.displayBooks()
+})
+
+
+
 book1 = new Book('TITLE','1925','234',true);
 book1.addBookToLibrary(myLibrary);
-myLibrary.displayBooks();
+
 book2 = new Book('TITLE2','1985','24',false);
 book2.addBookToLibrary(myLibrary);
 
-myLibrary.displayBooks();
