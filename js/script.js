9d01721fddee0f5003c589b290311ce99cdb5b0d
pvdberssel <pim.bersselaar@live.com>
2024-10-18 11:42:18 +0200

    Add function to library that displays objects

--- js/script.js
+++ js/script.js
@@ -7,10 +7,9 @@ function Book(bookTitle,bookAuthor,bookPages,bookRead){
     this.pages = bookPages;
     this.read = bookRead;
 }
-
-// function displayBooks(myLi){
-//     myLibrary
-// }
+myLibrary.displayBooks = function displayBooks(){
+    this.forEach((book) => console.log(book))
+}
 
 Book.prototype.addBookToLibrary = function addBookToLibrary(myLibrary){
     myLibrary.push(this)
@@ -21,23 +20,11 @@ Book.prototype.removeBookFromLibrary = function removeBookFromLibrary(myLibrary)
     myLibrary.splice(bookIndex,1)
 }
 
+myLibrary.displayBooks();
 book1 = new Book('TITLE','1925','234',true);
-// console.log(book1)
 book1.addBookToLibrary(myLibrary);
-
+myLibrary.displayBooks();
 book2 = new Book('TITLE2','1985','24',false);
 book2.addBookToLibrary(myLibrary);
-console.log(book2)
-console.log(myLibrary)
 
-book2.removeBookFromLibrary(myLibrary)
-console.log(myLibrary)
-
-book2.addBookToLibrary(myLibrary)
-console.log(myLibrary)
-book2.removeBookFromLibrary(myLibrary)
-console.log(myLibrary)
-book1.removeBookFromLibrary(myLibrary)
-console.log(myLibrary)
-book1.removeBookFromLibrary(myLibrary)
-console.log(myLibrary)
+myLibrary.displayBooks();
