f393b1f4784ecae3f3bd57a1ae655f92d1081d51
pvdberssel <pim.bersselaar@live.com>
2024-10-18 11:37:04 +0200

    Add remove object from array function

--- js/script.js
+++ js/script.js
@@ -8,19 +8,36 @@ function Book(bookTitle,bookAuthor,bookPages,bookRead){
     this.read = bookRead;
 }
 
-function displayBooks(myLi){
-    myLibrary
-}
+// function displayBooks(myLi){
+//     myLibrary
+// }
 
 Book.prototype.addBookToLibrary = function addBookToLibrary(myLibrary){
     myLibrary.push(this)
 }
 
+Book.prototype.removeBookFromLibrary = function removeBookFromLibrary(myLibrary){
+    var bookIndex = myLibrary.findIndex((book) => book.title == this.title,this)
+    myLibrary.splice(bookIndex,1)
+}
+
 book1 = new Book('TITLE','1925','234',true);
-console.log(book1)
+// console.log(book1)
 book1.addBookToLibrary(myLibrary);
-myLibrary.displayBooks();
+
 book2 = new Book('TITLE2','1985','24',false);
 book2.addBookToLibrary(myLibrary);
 console.log(book2)
-myLibrary.displayBooks();
+console.log(myLibrary)
+
+book2.removeBookFromLibrary(myLibrary)
+console.log(myLibrary)
+
+book2.addBookToLibrary(myLibrary)
+console.log(myLibrary)
+book2.removeBookFromLibrary(myLibrary)
+console.log(myLibrary)
+book1.removeBookFromLibrary(myLibrary)
+console.log(myLibrary)
+book1.removeBookFromLibrary(myLibrary)
+console.log(myLibrary)
